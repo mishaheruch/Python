d45d6ef9e3c42f973f9d765b745ff23580eff98c
Misga Herych <misha_gerich@icloud.com>
2025-12-21 20:45:48 +0200

    init

---
+++ test.py
@@ -0,0 +1,27 @@
+def debug_test():
+    x = 6
+    y = 10
+    z = x + y
+    z = 1 + z
+    
+    print("The value of x is:", x)
+    print("The value of y is:", y)
+    print("The value of z is:", z)
+
+def another_function():
+    a = 15
+    b = 25
+    c = a * b
+    
+    print("The value of a is:", a)
+    print("The value of b is:", b)
+    print("The value of c is:", c)
+
+another_function()
+
+debug_test()
+debug_test()
+debug_test()
+debug_test()
+debug_test()
+debug_test()
