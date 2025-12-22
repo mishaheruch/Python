ce756e0d3e8214ec9a4ea148cfaf33f51bd8b72f
Misga Herych <misha_gerich@icloud.com>
2025-12-22 16:39:39 +0200

    changed b to 50

---
+++ day01/age.py
@@ -0,0 +1,8 @@
+def int_age():
+    age = int(input('Скільки тобі років: ')) 
+    name = input('Як тебе звати: ')
+    age + 1
+    name = name + "!"
+    print("Привіт,", name, "Тобі через рік буде", age)
+
+int_age()
