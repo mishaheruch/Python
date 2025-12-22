8f4aa9dd57abc28c521151c87ccf7ed370530b85
Misga Herych <misha_gerich@icloud.com>
2025-12-22 19:21:09 +0200

    fix (x)

--- day01/age.py
+++ day01/age.py
@@ -1,7 +1,7 @@
 def int_age():
     age = int(input('Скільки тобі років: ')) 
-    name = input('Як тебе звати: ')
-    age + 1
+    name = input('Як тебе звати: ') 
+    age += 1
     name = name + "!"
     print("Привіт,", name, "Тобі через рік буде", age)
 
